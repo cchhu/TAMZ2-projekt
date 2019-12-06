a653e693ddb3b8ae44a4a96985aaef3d99a81d37
chu0077 <45642184+cchhu@users.noreply.github.com>
2019-12-07 00:24:32 +0100

    3. commit

--- app/src/main/java/com/example/hudebniprehravac/MainActivity.java
+++ app/src/main/java/com/example/hudebniprehravac/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.hudebniprehravac;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TableLayout;
+import androidx.appcompat.widget.Toolbar;
+
+
+import com.google.android.material.tabs.TabItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+    TableLayout tablayout;
+    TabItem actual;
+    TabItem all;
+    TabItem playlist;
+
+    @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar=(Toolbar)findViewById(R.id.tool_bar);
+        toolbar.setTitle("HUDEBNÍ PŘEHRÁVAČ");
+
+        tablayout=(TableLayout)findViewById(R.id.tabs);
+        actual=(TabItem)findViewById(R.id.just_playing);
+        all=(TabItem)findViewById(R.id.all_songs);
+        playlist=(TabItem)findViewById(R.id.play_list);
+
     }
+
 }
