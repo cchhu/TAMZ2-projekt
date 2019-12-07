326846c5e680ee8d460b9d9002ea184916e592e5
chu0077 <45642184+cchhu@users.noreply.github.com>
2019-12-07 15:14:13 +0100

    7. commit

--- app/src/main/java/com/example/hudebniprehravac/AllSongsFragment.java
+++ app/src/main/java/com/example/hudebniprehravac/AllSongsFragment.java
@@ -1,10 +1,13 @@
 package com.example.hudebniprehravac;
 
 
+import android.Manifest;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +15,12 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.example.hudebniprehravac.R;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -23,6 +32,7 @@ public class AllSongsFragment extends Fragment {
     ListView ListViewSongs;
     ArrayAdapter<String> ArrayAdapterSong;
     String songs[];
+    ArrayList<File> FileSongs;
 
     public AllSongsFragment() {
         // Required empty public constructor
@@ -35,21 +45,53 @@ public class AllSongsFragment extends Fragment {
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragment_all_songs, container, false);
         ListViewSongs=view.findViewById(R.id.ListViewSongs);
+
+
+        Dexter.withActivity(getActivity()).withPermission(Manifest.permission.READ_EXTERNAL_STORAGE).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse response) {
+
+                //noinspection deprecation
+                FileSongs=findSongs(Environment.getExternalStorageDirectory());
+            songs=new String[FileSongs.size()];
+
+            for(int i=0;i<FileSongs.size();i++){
+                songs[i]=FileSongs.get(i).getName();
+            }
+
+            ArrayAdapterSong=new ArrayAdapter<>(getActivity(),android.R.layout.simple_list_item_1,songs);
+            ListViewSongs.setAdapter(ArrayAdapterSong);
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse response) {
+
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+            token.continuePermissionRequest();
+            }
+        }).check();
         return view;
     }
 
     private ArrayList<File> findSongs(File file){
-
         ArrayList<File> allSongFiles=new ArrayList<>();
         File [] files=file.listFiles();
 
-        for(File f:files)
-        {
-            if(f.isDirectory() && !f.isHidden()){
-                allSongFiles.addAll(findSongs(f));
-            }
-            else {
-                allSongFiles.add(f);
+        Log.i("TEST","DSADSA"+files);
+
+
+        if (files != null) {
+            for(File f:files)
+            {
+                if(f.isDirectory() && !f.isHidden()){
+                    allSongFiles.addAll(findSongs(f));
+                }
+                else {
+                    allSongFiles.add(f);
+                }
             }
         }
         return allSongFiles;
